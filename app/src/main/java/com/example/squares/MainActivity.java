6cca91ed3058a809737a005cf0eac63bbdd79216
Danila Yarmarkin <danila.iarmarkin@gmail.com>
2020-02-17 18:49:05 +0300

    nothing

--- app/src/main/java/com/example/squares/MainActivity.java
+++ app/src/main/java/com/example/squares/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.squares;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private Button run,stop,next;
+    private TextView end, YourAnswers, RealAnswers;
+    private RadioButton xs,sqr;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
